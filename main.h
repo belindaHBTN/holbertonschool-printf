cb5a8acb655898538d410478f964fedce6312cb4
belindaHBTN <6372@holbertonstudents.com>
2023-03-28 02:24:08 -0700

    removed the test file, then added the main.h header file

---
+++ main.h
@@ -0,0 +1,10 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+int _putchar(char c);
+int print_char(char);
+int print_string(char *str);
+int print_percent();
+int _printf(const char *format, ...);
+
+#endif
