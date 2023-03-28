e074c3c7e99322ad03fed36f7dbe0deb5f5710f1
belindaHBTN <6372@holbertonstudents.com>
2023-03-28 02:58:07 -0700

    added print_string function to helper_func.c

--- helper_func.c
+++ helper_func.c
@@ -1,7 +1,21 @@
-#include <main.h>
+#include "main.h"
 
 int print_char(va_list ptr)
 {
     _putchar(va_arg(ptr, char));
     return (1);
+}
+
+int print_string(char *)
+{
+	int str_len;
+
+	str_len = 0;
+	while (str[str_len] != '\0')
+	{
+		_putchar(str[str_len]);
+		str_len = str_len + 1;
+	}
+
+	return (str_len);
 }
