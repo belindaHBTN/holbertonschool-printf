9d609f8039f32f1d3646de6c9e294441fd92c849
belindaHBTN <6372@holbertonstudents.com>
2023-03-28 23:11:43 -0700

    added comment, added a print_default function

--- helper_func.c
+++ helper_func.c
@@ -1,13 +1,30 @@
 #include "main.h"
+#include <stdarg.h>
 
+/**
+ * print_char - print the char
+ * @ptr: the char argument
+ *
+ * Description: print the char
+ * Return: the number of the input
+ */
 int print_char(va_list ptr)
 {
-    _putchar(va_arg(ptr, char));
+    _putchar(va_arg(ptr, int));
+
     return (1);
 }
 
-int print_string(char *str)
+/**
+ * print_string - print the string to the stdout
+ * @str: the string to be printed
+ *
+ * Description: print the string to the stdout
+ * Return: the number of character in the string
+ */
+int print_string(va_list ptr)
 {
+	char *str = va_arg(ptr, char *);
 	int str_len;
 
 	str_len = 0;
@@ -20,9 +37,28 @@ int print_string(char *str)
 	return (str_len);
 }
 
-int print_percent()
+/**
+ * print_percent - print the %
+ * @ptr: will not be used
+ *
+ * Description: print the %
+ * Return: the len of % which is 1
+ */
+int print_percent(__attribute__ ((unused)) va_list ptr)
 {
 	_putchar('%');
 
 	return (1);
+}
+
+/**
+ * print_default - print nothing, a function to be returned
+ * ptr: will not be used
+ *
+ * Description: print nothing, a function to be returned
+ * Return: 2
+ */
+int print_default(__attribute__ ((unused)) va_list ptr)
+{
+	return (2);
 }
