10d77fa68baee149544037cd7db0b4fc5eb39cb4
belindaHBTN <6372@holbertonstudents.com>
2023-03-28 18:33:28 -0700

    added struct and prototype other helper functions into main.h

--- main.h
+++ main.h
@@ -1,10 +1,19 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
-int _putchar(char c);
-int print_char(char);
-int print_string(char *str);
-int print_percent();
+struct pr_type
+{
+	char *fmtp;
+	int (*f)(va_list ptr);
+};
+typedef struct pr_type pr_t;
+
+int _putchar(va_list ptr);
+int print_char(va_list ptr);
+int print_string(va_list ptr);
+int print_percent(va_list ptr);
+int print_default(va_list ptr);
+int (*get_type_func(char sp))(va_list ptr);
 int _printf(const char *format, ...);
 
 #endif
